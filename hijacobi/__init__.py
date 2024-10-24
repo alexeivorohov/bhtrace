739a54356b6a0e7a7f27d1859eef93203fa63f6a
alexeivorohov <alexei.vorohov@yandex.ru>
2024-10-25 00:36:34 +0300

    deleted:    hijacobi/__init__.py 	deleted:    hijacobi/dev/particle.py 	deleted:    hijacobi/emit.py 	deleted:    hijacobi/imagen.py 	deleted:    hijacobi/model.py 	deleted:    hijacobi/routines.py 	deleted:    hijacobi/solveieq.py 	deleted:    hijacobi/task.py 	deleted:    hijacobi/tracer.py 	modified:   requirements.txt 	deleted:    results/EH_Geod_045.png 	deleted:    results/EH_I2.png 	deleted:    results/EH_lens_045.png 	deleted:    results/RN_I2.png 	deleted:    results/RN_geod.png 	deleted:    results/RN_lens.png 	deleted:    results/S_Geod.png 	deleted:    results/S_lens.png 	deleted:    to_merge/BH_Images_postproc.ipynb

--- hijacobi/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Torch-based library for modeling images of compact objects 
-
-"""
-from hijacobi.model import *
-from hijacobi.solver import *
-from hijacobi.task import *
-from hijacobi.tracer import *
-from hijacobi.emit import *
