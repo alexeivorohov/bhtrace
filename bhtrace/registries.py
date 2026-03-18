fe3932183f3e018a4698830077965f85a9250eca
av <alexeivorohov@yandex.ru>
2026-03-18 15:54:30 +0300

    refactor: exposed registries in one place

---
+++ bhtrace/registries.py
@@ -0,0 +1,9 @@
+"""
+This module exposes all project registries
+"""
+
+from bhtrace.utils.diff import DIFF_SCHEME_REGISTRY
+
+from bhtrace.geometry import SPACETIME_REGISTRY, PARTICLE_REGISTRY, ELECTRODYNAMICS_REGISTRY
+from bhtrace.medium import MEDIUM_REGISTRY
+from bhtrace.grrt import RADIATIVE_MODEL_REGISTRY
