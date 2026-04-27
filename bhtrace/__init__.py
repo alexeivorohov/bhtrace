d18f436bf10c28c5fbb5be1d7e1540abcdacac27
av <alexeivorohov@yandex.ru>
2026-04-27 03:45:54 +0300

    refactor: updated package-level imports

--- bhtrace/__init__.py
+++ bhtrace/__init__.py
@@ -1,12 +1,18 @@
 """
 PyTorch-based library for modeling images of compact objects 
-
-
 """
-# About variable notation:
-# 1. We use _ to distinguish already computed quantities from computation methods (e.g. F vs _F, g vs _g and etc.)
-# 2. For tensors, contravariant (vector) indexes come after variable, covariant - before variable (e.g. ik_g vs g_ik)
-# 3. As ususal, we use i, k, j, m, n, l letters to designate spatial indexes and u, v, w, p, q letters for spacetime indexes. Common batch and time slice indexes are b and N.
+
+# --- Public API ---
+
+from . import geometry
+from . import graphics
+from . import grrt
+from . import medium
+from . import scenarios
+from . import tracing
+from . import trajectory
+from . import utils
+
+# --- Submodules ---
 
 from .trajectory import Trajectory
-
