10b6aad30b37d2f2f35c91e382ed7fb99f21cc49
alexeivorohov <alexeivorohov@yandex.ru>
2026-05-26 19:26:25 +0300

    fix: updated top-level imports

--- bhtrace/__init__.py
+++ bhtrace/__init__.py
@@ -4,15 +4,27 @@ PyTorch-based library for modeling images of compact objects
 
 # --- Public API ---
 
+# Submodules
+
 from . import geometry
 from . import graphics
 from . import grrt
 from . import medium
 from . import scenarios
 from . import tracing
-from . import trajectory
+from . import data
 from . import utils
+from . import globs
+from . import registries
 
-# --- Submodules ---
 
-from .trajectory import Trajectory
+# Data structures
+
+from .data import Trajectory, RunningTensor, GRRTData
+
+# Base classes
+from .geometry import Spacetime, Particle
+from .medium import Medium
+from .grrt import RadiativeModel
+from .tracing import Tracer
+
