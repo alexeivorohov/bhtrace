8118a64393647994db8852326195d6115a8b209f
alexeivorohov <alexei.vorohov@yandex.ru>
2026-05-31 21:11:15 +0300

    refactor: cleaned up `globs`  and `utils.operation` modules

--- bhtrace/globs.py
+++ bhtrace/globs.py
@@ -1,76 +1,73 @@
 """
-Defines and initializes project-wide global constants and objects.
+Defines and initializes project-wide global constants, paths, and configuration 
+objects for BHTrace.
 
-Key Globals:
-- `CFG_DIR`: Path to the main configuration directory.
-- `EPS`: Numeric precision t
+This module centralizes system settings such as numeric precision, logging output 
+paths, and persistent storage directories (e.g., model configurations and output results). 
+Configuration variables can be overridden via environment variables to facilitate 
+flexible execution in different environments.
+
+Attributes
+----------
+EPS : float, defaults to 1e-4
+    Default numeric precision throughout the project.
+    Controlled by environment variable `BHTRACE_NUMERIC_EPS`.
+
+TRAJECTORY_COORD_REPR_CACHE_SIZE : int, defaults to 2
+    Sets the uppper limit on how much coordinate representations
+    for Trajectory object to store.
+    Controlled by environment variable `BHTRACE_TRAJ_CACHE_SIZE`.
+
+LOG_FACTORY_PARAMS : bool, defaults to False
+    Controls whether creation parameters for main objects (Particles, Spacetimes ...) 
+    should be outputted to log file.
+    Controlled by environment variable `BHTRACE_LOG_FACTORY_PARAMS`.
+
+LOG_FILE : pathlib.Path,
+    Path where application logs are written. If not set, logs are outputted only to console.
+    Controlled by environment variable `BHTRACE_LOG_FILE`    
+
+CFG_DIR : pathlib.Path, defaults to `.config`
+    Path to the main configuration directory, containing yaml configurations of the runs.
+    By default points to configurations pre-installed with the package.
+    Controlled by environment variable `BHTRACE_CFG_DIR`.
+    (TBD: will only be used in future by bhtrace-cli interface)
+
+OUTPUT_DIR: pathlib.Path, defaults to `pathlib.Path.cwd()`
+    Default directory for saving all output data (results, checkpoints, etc.). 
+    Controlled by BHTRACE_OUTPUT_DIR, or defaults to the current working directory.
+
+_DO_SAVE_TEST_OUTPUTS : bool, defaults to False
+    Controls whether plots created during test runs should be saved.
+    Useful for reducing disk write operations.
 
 """
 import os
-import math
 import pathlib
 import logging
-from typing import Any, NamedTuple
+from bhtrace.utils.operation import _env_var_path_override
 
-import sympy as sp
-import sympy.physics.units as units
-
-
-# --- Logging ---
 
 log = logging.getLogger(__name__)
 
+# --- Flags ---
+
+LOG_FACTORY_PARAMS: bool = bool(os.environ.get('BHTRACE_LOG_FACTORY_PARAMS', False))
+_DO_SAVE_TEST_OUTPUTS: bool = bool(os.environ.get('BHTRACE_DO_SAVE_TEST_OUTPUTS', False))
+
 # --- Constants ---
 
-planck_time = sp.sqrt(units.hbar*units.G/units.c**5)
-planck_length = sp.sqrt(units.hbar*units.G/units.c**3)
-planck_mass = sp.sqrt(units.hbar*units.c/units.G)
-planck_temperature = sp.sqrt(units.hbar*units.c**5/(units.G*units.boltzmann**2))
-
-planck_subs = {
-    units.G: 1.0, units.c: 1.0, units.hbar: 1.0, units.boltzmann: 1.0,
-}
-
-si_subs = {
-    units.G: float(units.G), units.c: float(units.c), 
-    units.hbar: float(units.hbar), units.boltzmann: float(units.boltzmann),
-}
-
-planck_system = [units.G, units.c, units.hbar, units.boltzmann]
-
-def to_planck_units(expr: Any) -> float:
-    return units.convert_to(expr, planck_system).subs(planck_subs)
-
-EPS = os.environ.get('BHTRACE_NUMERIC_EPS', 1e-5)
+EPS: float = float(os.environ.get('BHTRACE_NUMERIC_EPS', '1e-4'))
+TRAJECTORY_COORD_REPR_CACHE_SIZE: int = int(os.environ.get("BHTRACE_TRAJ_CACHE_SIZE", 4))
 
 # --- Paths ---
 
-def _env_var_path_override(name: str, is_dir: bool = False) -> str | None:
+CFG_DIR: pathlib.Path = _env_var_path_override("BHTRACE_CFG_DIR", is_dir=True, log=log) or pathlib.Path(__file__).parent / 'config'
+# log.info(f'Using configuration directory {CFG_DIR.absolute()}')
 
-    path = os.environ.get(name)
-    if path is None:
-        return None
+LOG_FILE: pathlib.Path | None = _env_var_path_override("BHTRACE_LOG_FILE", log=log)
+if LOG_FILE:
+    log.info(f"Outputting logs to {LOG_FILE}")
 
-    path = pathlib.Path(path)
-    assert path.exists(),\
-        f"Detected path override by environment variable {name}, but path does not exist {path}"
-    
-    if is_dir:
-        assert path.is_dir(),\
-            f"Path override by environment variable {name} is not a directory: {path}"
-    else:
-        assert path.is_file(),\
-            f"Path override by environment variable {name} is not a file: {path}"
-
-    log.info(
-        f"Successfully read path override from environment variable {name}, "
-        f"set {name} to {path}"
-    )
-
-    return path
-
-# --- Path Definitions ---
-
-# The primary configuration directory. Can be overridden by the BHTRACE_CFG_DIR env var.
-CFG_DIR =  _env_var_path_override("BHTRACE_CFG_DIR", True) or pathlib.Path(__file__).parent / 'config'
-log.info(f'Using configuration directory {CFG_DIR.absolute()}')
+OUTPUT_DIR: pathlib.Path = _env_var_path_override("BHTRACE_OUTPUT_DIR", is_dir=True, log=log) or pathlib.Path.cwd()
+log.info(f"Default output directory: {OUTPUT_DIR}")
