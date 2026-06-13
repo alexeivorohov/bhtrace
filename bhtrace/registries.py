684168d5a84456873d85696ff9aa93e97c75d01c
alexeivorohov <alexei.vorohov@yandex.ru>
2026-06-13 23:05:37 +0300

    fix: removed old import

--- bhtrace/registries.py
+++ bhtrace/registries.py
@@ -3,7 +3,7 @@ This module exposes all project registries
 """
 
 from bhtrace.utils.diff import DIFF_SCHEME_REGISTRY
-
-from bhtrace.geometry import SPACETIME_REGISTRY, PARTICLE_REGISTRY, ELECTRODYNAMICS_REGISTRY
+from bhtrace.geometry import SPACETIME_REGISTRY, PARTICLE_REGISTRY
+from bhtrace.physics.electrodynamics import ELECTRODYNAMICS_REGISTRY
 from bhtrace.medium import MEDIUM_REGISTRY
 from bhtrace.grrt import RADIATIVE_MODEL_REGISTRY
